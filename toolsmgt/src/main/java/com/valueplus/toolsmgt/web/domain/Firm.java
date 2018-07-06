299d048b42e206af6fd2b8b31020765e85d64469
wangzihao <zaza5217@163.com>
2018-07-06 13:56:24 +0800

    com1

---
+++ toolsmgt/src/main/java/com/valueplus/toolsmgt/web/domain/Firm.java
@@ -0,0 +1,50 @@
+package com.valueplus.toolsmgt.web.domain;
+
+public class Firm {
+    private Integer id;
+    private String code;
+    private String name;
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Firm{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
---
+++ toolsmgt/src/main/java/com/valueplus/toolsmgt/web/domain/Permission.java
@@ -0,0 +1,40 @@
+package com.valueplus.toolsmgt.web.domain;
+
+public class Permission {
+    private Integer id;
+    private String code;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Permission{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
